a973915470e6727aa7c8d2625992e6fe38f1499d
Noeal Rajeev <noealrajeev987@gmail.com>
2024-12-26 20:11:00 +0530

    Custom colors configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,10 +9,14 @@ export default {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "gradient-bg":
+          "linear-gradient(339deg,rgba(0, 0, 0, 0) 11.27%,rgba(171, 44, 169, 0.2) 82.75% ),radial-gradient(41.38% 20.56% at 92.19% 62.7%, #280e3b 0%, #000 57%)",
+        "text-gradient":
+          "linear-gradient(93deg, rgba(255, 255, 255, 0.82) 5.48%, rgba(59, 20, 87, 0.82) 97.88%)",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        purple: {
+        _purple: {
           400: "#B186C8", // Slightly lighter than 500
           500: "#9E6FB1", // Base color
           600: "#8B619E", // Slightly darker than 500
