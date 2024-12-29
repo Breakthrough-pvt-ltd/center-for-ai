9b594ff95bb7fce1204fb944567ec261630730fa
Noeal Rajeev <noealrajeev987@gmail.com>
2024-12-29 23:02:43 +0530

    tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,8 +12,12 @@ export default {
       backgroundImage: {
         "gradient-bg":
           "linear-gradient(339deg,rgba(0, 0, 0, 0) 11.27%,rgba(171, 44, 169, 0.2) 82.75% ),radial-gradient(41.38% 20.56% at 92.19% 62.7%, #280e3b 0%, #000 57%)",
+        "card-gradient-bg":
+          "linear-gradient(0,rgba(41, 10, 45, 1) 100%,rgba(0, 0, 0, 1) 100% )",
         "text-gradient":
           "linear-gradient(93deg, rgba(255, 255, 255, 0.82) 5.48%, rgba(59, 20, 87, 0.82) 97.88%)",
+        "text-gradient-sub":
+          "linear-gradient(90deg, rgba(200, 153, 219, 0.69) 0.48%, rgba(69, 46, 100, 0.71) 100%)",
       },
       colors: {
         _purple: {
@@ -22,7 +26,7 @@ export default {
           600: "#8B619E", // Slightly darker than 500
           700: "#794E8A", // More pronounced dark shade
           800: "#663A76", // Darker shade
-          900: "#542862", // Deepest shade
+          900: "#280A2C", // Deepest shade
         },
         black: "#000000",
       },
