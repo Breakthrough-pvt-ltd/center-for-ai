d213b2de3ffab0e1b18bcad03e88d7c0d6e7c215
Noeal Rajeev <noealrajeev987@gmail.com>
2025-01-04 09:37:50 +0530

    teams page padding

--- src/app/(routes)/team/page.tsx
+++ src/app/(routes)/team/page.tsx
@@ -4,7 +4,7 @@ export default function Team() {
       <div className="relative bg-gradient-bg min-h-screen">
         {/* Title Section */}
         <div className="flex items-center justify-start px-8 pt-36 pl-36">
-          <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold leading-tight bg-clip-text text-transparent bg-text-gradient-sub max-w-lg">
+          <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold leading-tight bg-clip-text text-transparent bg-text-gradient-sub max-w-lg pb-4">
             | Team
           </h1>
         </div>
